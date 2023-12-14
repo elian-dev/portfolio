993775f459e6d8561124a6fa3048d31644233a1e
elian-dev <geordymemdoza@gmail.com>
2023-12-13 21:38:25 -0600

    fix: :bug: Fix vercel analytics pkg

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
